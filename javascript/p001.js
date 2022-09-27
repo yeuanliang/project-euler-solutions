bd56b2bbe17fecd7efb6beb99f1c59a3a88b4da6
zizaiziran <zizaiziran@gmail.com>
2022-09-27 10:58:31 +0800

    refactor

--- javascript/p001.js
+++ javascript/p001.js
@@ -1,22 +1,10 @@
 "use strict";
 
-let sum_3x = 0;
-let sum_5x = 0;
-let sum_15x = 0;
+const target = 999;
 let result = 0;
-
-for (let i = 3; i < 1000; i = i + 3) {
-  sum_3x += i;
-}
-
-for (let i = 5; i < 1000; i = i + 5) {
-  sum_3x += i;
-}
-
-for (let i = 15; i < 1000; i = i + 15) {
-  sum_15x += i;
-}
-
-result = sum_3x + sum_5x - sum_15x;
-
+const sumDivisibleBy = function (n) {
+  const p = parseInt(target / n);
+  return (n * p * (p + 1)) / 2;
+};
+result = sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15);
 console.log(result);
