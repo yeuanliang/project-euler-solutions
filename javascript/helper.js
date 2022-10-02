fcc96bcb8454cef9733044def204faceb5b612a0
zizaiziran <zizaiziran@gmail.com>
2022-10-02 09:14:48 +0800

    fix bug

--- javascript/helper.js
+++ javascript/helper.js
@@ -35,7 +35,7 @@ exports.isPrime = internals.isPrime = function (num) {
   if (num % 2 === 0 || num % 3 === 0) {
     return false;
   }
-  for (let i = 5; i * i < num; i += 6) {
+  for (let i = 5; i * i <= num; i += 6) {
     if (num % i === 0 || num % (i + 2) === 0) {
       return false;
     }
