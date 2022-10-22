5cc56ef86e981e2f4d868745c0e1acf9ef7c699d
zizaiziran <zizaiziran@gmail.com>
2022-10-22 14:10:11 +0800

    new

---
+++ javascript/algorithms/combination.js
@@ -0,0 +1,19 @@
+let res = [];
+function combinations(arr, k, start, idx, current) {
+  if (idx === k) {
+    res.push(current.slice());
+    return;
+  }
+  for (var i = start; i < arr.length; i += 1) {
+    current[idx] = arr[i];
+    combinations(arr, k, i + 1, idx + 1, current);
+  }
+}
+return function (arr, k) {
+  res = [];
+  combinations(arr, k, 0, 0, []);
+  var temp = res;
+  // Free the extra memory
+  res = null;
+  return temp;
+};
