c325b533fa492e342dd0bad2cd5c9c4099440a60
zizaiziran <zizaiziran@gmail.com>
2022-10-31 16:28:26 +0800

    refactor

--- javascript/p086.js
+++ javascript/p086.js
@@ -24,7 +24,8 @@ const countSolutions = function (n) {
 };
 
 const count = function (m) {
-  let cuboids = new Set();
+//   let cuboids=[]
+  let count=0
   for (let i = 1; i < m; i++) {
     for (let j = i + 1; j < m; j++) {
       if (helper.gcd(i, j) !== 1) {
@@ -32,45 +33,43 @@ const count = function (m) {
       }
       let s = Math.min(j * j - i * i, 2 * i * j);
       let t = Math.max(j * j - i * i, 2 * i * j);
-      if (helper.gcd(s, t) !== 1) {
-        continue;
-      }
       if (s + t > 3 * m) {
         break;
       }
+      if (helper.gcd(s, t) !== 1) {
+        continue;
+      }
       let k = 1;
       while (k * s <= m && k * t <= 2 * m) {
-        for (let a = 1; a <= m; a++) {
-          if (k * t - a > m) {
-            continue;
-          }
-          if (k * t - a < 1) {
-            break;
-          }
+        for (let a = 1; a <= Math.floor(k*t/2); a++) {
           if (k * s >= a && k * s >= k * t - a) {
-            cuboids.add([k * s, a, k * t - a].sort((x, y) => x - y).join(" "));
+            // cuboids.push(a + " " + (k * t - a) + " " + k * s);
+            count++
           }
         }
         if (k * t <= m) {
-          for (let a = 1; a <= Math.floor(k*s/2); a++) {
-            cuboids.add([k * t, k * s - a, a].sort((x, y) => x - y).join(" "));
+          for (let a = 1; a <= Math.floor((k * s) / 2); a++) {
+            // cuboids.push(a + " " + (k * s - a) + " " + k * t);
+            count++
           }
         }
         k++;
       }
     }
   }
-  return cuboids.size;
+  return count;
 };
 
 const p086Solution = function () {
   let target = 1000000;
-  let start = 1800;
+  let start = 100;
   while (count(start) <= target) {
-    start++;
+    start+=20;
   }
-  return start;
+  while (count(start) > target) {
+    start--;
+  }
+  return start+1;
 };
+
 console.log(p086Solution())
-// console.log(count(2000))
-// console.log(countSolutions(2000));
