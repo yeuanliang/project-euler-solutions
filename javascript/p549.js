6daa41cca4e41e855368d8012da3b0236ad439d7
zizaiziran <zizaiziran@gmail.com>
2022-11-06 14:51:42 +0800

    refactor

--- javascript/p549.js
+++ javascript/p549.js
@@ -1,49 +1,81 @@
-'use strict'
+"use strict";
 
-const helper = require('./helper')
+const helper = require("./helper");
+const limit = 10 ** 8;
+const rootLimit = Math.floor(Math.sqrt(limit));
+const primesSet = helper.getPrimes(rootLimit);
+const mMap = new Map();
 
-const divisibilityFactorial = function(base,exp){
-    if(base>10){
-        return base*exp
-    }
-    let m = base
-    let t=1
-    while(t<exp){
-        m+=base
-        let temp=m
-        while(temp%base===0){
-            t+=1
-            temp=temp/base
+const primeFactorization = function (n) {
+    const bases = [];
+    const exponents = [];
+    for (let i = 0; primesSet[i] * primesSet[i] <= n; ++i) {
+      if (n % primesSet[i] == 0) {
+        let count = 0;
+        while (n % primesSet[i] == 0) {
+          n /= primesSet[i];
+          count += 1;
         }
+        bases.push(primesSet[i]);
+        exponents.push(count);
+      }
     }
-    return m
-}
+    if (n > 1) {
+      bases.push(n);
+      exponents.push(1);
+    }
+    return { bases, exponents };
+  };
 
-const smallestFactorial = function(n){
-    if(helper.isPrime(n)){
-        return n
+const countM = function () {
+  let primes = [2, 3, 5, 7];
+  for (const p of primes) {
+    let e = 0;
+    let m = 0;
+    while (p ** e <= limit) {
+      m += p;
+      let t = m;
+      while (t % p === 0) {
+        t = t / p;
+        e += 1;
+        mMap.set(p + " " + e, m);
+      }
     }
-    const {bases,exponents} = helper.getDivisors(n)
-    const len = bases.length
-    let m=0
-    for(let i=len-1;i>=0;i--){
-        if(bases[i]*exponents[i]<m){
-            continue
-        }
-        let temp = divisibilityFactorial(bases[i],exponents[i])
-        if(m<temp){
-            m=temp
-        }
-    }
-    return m
-}
+  }
+};
 
-const p549Solution = function(limit){
-    let sum = 0
-    for(let i=2;i<=limit;i++){
-        sum+=smallestFactorial(i)
-    }
-    return sum
-}
+countM()
 
-console.log(p549Solution(10**8))
+const smallestFactorial = function (n) {
+  if (helper.isPrime(n)) {
+    return n;
+  }
+  const { bases, exponents } = primeFactorization(n);
+  const len = bases.length;
+  let m = 0;
+  for (let i = len - 1; i >= 0; i--) {
+    if(bases[i] * exponents[i]<m){
+      continue
+    }
+    let temp = 0;
+    if(bases[i]>10){
+        temp = bases[i] * exponents[i]
+    }else{
+        temp = mMap.get(bases[i]+' '+exponents[i])
+    }
+    if (m < temp) {
+      m = temp;
+    }
+  }
+  return m;
+};
+
+const p549Solution = function () {
+  let sum = 0;
+  for (let i = 2; i <= limit; i++) {
+    sum += smallestFactorial(i);
+  }
+  return sum;
+};
+
+console.log(p549Solution())
