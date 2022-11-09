ebedcf9399c6b2c930fd4e0162e599f74aacfc83
zizaiziran <zizaiziran@gmail.com>
2022-11-09 09:04:11 +0800

    refactor

--- javascript/p719.js
+++ javascript/p719.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// time cost: 24min43sec
+// time cost: 8min8sec
 
 const findCombinations = function (s, combinations, substring = []) {
   if (!s) {
@@ -23,6 +23,29 @@ const findAllCombinations = function (s) {
   return combinations;
 };
 
+const isS = function (square, root) {
+  const s = square + "";
+  const len = s.length;
+  for (let i = 0; i < 1 << (len - 1); i++) {
+    let c = [];
+    let ss = s;
+    let sum = 0;
+    for (let j = 1; j < len; j++) {
+      if (i & (1 << (j - 1))) {
+        sum += +ss.slice(0, j - (len - ss.length));
+        if (sum > root) {
+          break;
+        }
+        ss = ss.slice(j - (len - ss.length));
+      }
+    }
+    sum += +ss;
+    if (sum === root) {
+      return true;
+    }
+  }
+  return false;
+};
 const isSNumber = function (square, root) {
   const combinations = findAllCombinations(square + "");
   const filter = function (arr) {
@@ -46,11 +69,11 @@ const p719Solution = function (limit) {
   let sum = 0;
   let r = Math.floor(Math.sqrt(limit));
   for (let i = 2; i <= r; i++) {
-    if (isSNumber(i * i, i)) {
+    if (isS(i * i, i)) {
       sum += i * i;
     }
   }
   return sum;
 };
 
-// console.log(p719Solution(10**12))
+console.log(p719Solution(10 ** 12));
