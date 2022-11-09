784ac8748c6c4477597cc5875e1feebf222cf90d
zizaiziran <zizaiziran@gmail.com>
2022-11-09 09:05:47 +0800

    refactor

--- javascript/p719.js
+++ javascript/p719.js
@@ -23,29 +23,7 @@ const findAllCombinations = function (s) {
   return combinations;
 };
 
-const isS = function (square, root) {
-  const s = square + "";
-  const len = s.length;
-  for (let i = 0; i < 1 << (len - 1); i++) {
-    let c = [];
-    let ss = s;
-    let sum = 0;
-    for (let j = 1; j < len; j++) {
-      if (i & (1 << (j - 1))) {
-        sum += +ss.slice(0, j - (len - ss.length));
-        if (sum > root) {
-          break;
-        }
-        ss = ss.slice(j - (len - ss.length));
-      }
-    }
-    sum += +ss;
-    if (sum === root) {
-      return true;
-    }
-  }
-  return false;
-};
+
 const isSNumber = function (square, root) {
   const combinations = findAllCombinations(square + "");
   const filter = function (arr) {
@@ -59,6 +37,29 @@ const isSNumber = function (square, root) {
   const numberArrays = combinations.filter((item) => filter(item));
   for (const numberArray of numberArrays) {
     if (numberArray.reduce((x, y) => x + y) === root) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const isS = function (square, root) {
+  const s = square + "";
+  const len = s.length;
+  for (let i = 0; i < 1 << (len - 1); i++) {
+    let ss = s;
+    let sum = 0;
+    for (let j = 1; j < len; j++) {
+      if (i & (1 << (j - 1))) {
+        sum += +ss.slice(0, j - (len - ss.length));
+        if (sum > root) {
+          break;
+        }
+        ss = ss.slice(j - (len - ss.length));
+      }
+    }
+    sum += +ss;
+    if (sum === root) {
       return true;
     }
   }
