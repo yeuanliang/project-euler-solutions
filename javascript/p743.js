ba0cc353295d9fd286795ea4302e3735afa3875d
zizaiziran <zizaiziran@gmail.com>
2022-11-14 17:32:14 +0800

    refactor

--- javascript/p743.js
+++ javascript/p743.js
@@ -1,8 +1,9 @@
 "use strict";
-// time cost: 9min13sec
+// time cost: 1min13sec
 const helper = require("./helper");
 //https://www.zybuluo.com/ArrowLLL/note/713749
 // A(m,n) = c(i,2i)*c(2i,m)*2**(n-2*i*m) i = [0 , m/2], n = m * m
+// f(i+1) = f(i)*(m-2*i)*(m-2*i-1)/((i+1)*(i+1)*2**(2*m))
 const p = 10n ** 9n + 7n;
 const n = 10n ** 16n;
 const m = 10n ** 8n;
