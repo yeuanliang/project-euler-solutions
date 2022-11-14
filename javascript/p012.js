c85f32acbaf296953b4669fcffdea6685c4095d7
zizaiziran <zizaiziran@gmail.com>
2022-11-14 08:26:20 +0800

    refactor

--- javascript/p012.js
+++ javascript/p012.js
@@ -2,23 +2,22 @@
 
 const helper = require("./helper");
 
-const firstTriangularNumberHasNDivisors = function(divisorsCount){
-    let i = 3
-    while(true){
-        let triangularNumber = i*(i+1)/2
-        let count = 1
-        if((i+1)%2===0){
-            count = helper.getDivisors((i+1)/2).count * helper.getDivisors(i).count
-        }else{
-            count = helper.getDivisors(i/2).count * helper.getDivisors(i+1).count
-        }
-        if(count>divisorsCount){
-            return triangularNumber
-            break
-        }else{
-            i=i+1
-        }
-    } 
-}
+const firstTriangularNumberHasNDivisors = function (divisorsCount) {
+  let i = 3;
+  while (true) {
+    let triangularNumber = (i * (i + 1)) / 2;
+    let count = 1;
+    if ((i + 1) % 2 === 0) {
+      count = helper.countDivisors((i + 1) / 2) * helper.countDivisors(i);
+    } else {
+      count = helper.countDivisors(i / 2) * helper.countDivisors(i + 1);
+    }
+    if (count > divisorsCount) {
+      return triangularNumber;
+    } else {
+      i = i + 1;
+    }
+  }
+};
 
-console.log(firstTriangularNumberHasNDivisors(500))
+console.log(firstTriangularNumberHasNDivisors(500));
--- javascript/p047.js
+++ javascript/p047.js
@@ -4,10 +4,10 @@ const helper = require("./helper");
 
 const p047Solution = function () {
   for (let i = 647; ; i++) {
-    if (helper.getDivisors(i).bases.length === 4) {
-      if (helper.getDivisors(i + 1).bases.length === 4) {
-        if (helper.getDivisors(i + 2).bases.length === 4) {
-          if (helper.getDivisors(i + 3).bases.length === 4) {
+    if (helper.primeFactorization(i).bases.length === 4) {
+      if (helper.primeFactorization(i + 1).bases.length === 4) {
+        if (helper.primeFactorization(i + 2).bases.length === 4) {
+          if (helper.primeFactorization(i + 3).bases.length === 4) {
             return i;
           }
         }
