abad7579aee427b7a8e97ba0ee90f96fa12fdfd0
zizaiziran <zizaiziran@gmail.com>
2022-11-19 18:24:58 +0800

    change style

--- javascript/p131.js
+++ javascript/p131.js
@@ -1,25 +1,25 @@
-'use strict'
+"use strict";
 
-const helper = require('./helper')
+const helper = require("./helper");
 
 // n^3 + n^2 * p = m^3, p = (m - n)((m / n + 1/2)^2 + 3/4)
 // m = s * n / t, p = n * (s - t) * (s^2 + s * t + t^2) / t^3
 // s - t = 1, n = t^3, p = 3 * t^2 + 3 * t + 1
 
-const p131Solution = function(limit){
-    let count = 0
-    let t = 1
-    while(true){
-        let p = 3*t*t+3*t+1
-        if(p>limit){
-            break
-        }
-        if(helper.isPrime(p)){
-            count++
-        }
-        t++
+const p131Solution = function (limit) {
+  let count = 0;
+  let t = 1;
+  while (true) {
+    let p = 3 * t * t + 3 * t + 1;
+    if (p > limit) {
+      break;
     }
-    return count
-}
+    if (helper.isPrime(p)) {
+      count++;
+    }
+    t++;
+  }
+  return count;
+};
 
-console.log(p131Solution(10**6))
+console.log(p131Solution(10 ** 6));
