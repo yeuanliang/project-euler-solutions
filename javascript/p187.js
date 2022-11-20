c5e1240bef9974e6317f56dcebad7cb9244854be
zizaiziran <zizaiziran@gmail.com>
2022-11-20 20:31:39 +0800

    new solution

---
+++ javascript/p187.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const helper = require('./helper')
+
+const p187Solution = function(limit){
+    const primes = helper.getPrimes(Math.floor(limit/2))
+    const len = primes.length
+    let count = 0
+    for(let i=0;i<len;i++){
+        for(let j=i;j<len;j++){
+            if(primes[i]*primes[j]<limit){
+                count++
+            }else{
+                break
+            }
+            
+        }
+    }
+    return count
+}
+
+console.log(p187Solution(10**8))
