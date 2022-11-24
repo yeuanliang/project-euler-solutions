1404f329d2ec695836013f5c8647c9df93d2daab
zizaiziran <zizaiziran@gmail.com>
2022-11-24 09:11:57 +0800

    new solution

---
+++ javascript/p134.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// time cost: 2m 9s
+
+const helper = require("./helper");
+
+const findNumber = function (p1, p2) {
+  const exp = (p1 + "").length;
+  let i = 3;
+  while (true) {
+    let t = p2 * i;
+    if (t % 10 ** exp === p1) {
+      return t;
+    }
+    i += 2;
+    if(p1!==5&&i%5===0){
+        i+=2
+    }
+  }
+};
+
+const p134Solution = function () {
+  const primes = helper.getPrimes(1000100);
+  const p = primes.slice(2);
+  let i=0
+  let sum=0n
+  while(p[i]<1000000){
+    let t = findNumber(p[i],p[i+1])
+    sum+=BigInt(t)
+    i++
+  }
+  return sum
+};
+
+console.log(p134Solution());
+
