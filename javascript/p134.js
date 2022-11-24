e6c11b18fac3f35c70568b8d142c6909a5d51cef
zizaiziran <zizaiziran@gmail.com>
2022-11-24 10:21:23 +0800

    refactor

--- javascript/p134.js
+++ javascript/p134.js
@@ -4,33 +4,45 @@
 
 const helper = require("./helper");
 
+// const findNumber = function (p1, p2) {
+//   const exp = (p1 + "").length;
+//   let i = 3;
+//   while (true) {
+//     let t = p2 * i;
+//     if (t % 10 ** exp === p1) {
+//       return t;
+//     }
+//     i += 2;
+//     if(p1!==5&&i%5===0){
+//         i+=2
+//     }
+//   }
+// };
+
 const findNumber = function (p1, p2) {
   const exp = (p1 + "").length;
-  let i = 3;
-  while (true) {
-    let t = p2 * i;
-    if (t % 10 ** exp === p1) {
-      return t;
-    }
-    i += 2;
-    if(p1!==5&&i%5===0){
-        i+=2
-    }
+  const r1 = p2 - p1;
+  const r2 = 10 ** exp % p2;
+  // r2*k ≡ r1 (mod p2)
+  let y = helper.extgcd(p2, r2)["y"];
+  if (y < 0) {
+    y += p2;
   }
+  let t = ((r1 * y) % p2) * 10 ** exp + p1;
+  return t;
 };
 
 const p134Solution = function () {
   const primes = helper.getPrimes(1000100);
   const p = primes.slice(2);
-  let i=0
-  let sum=0n
-  while(p[i]<1000000){
-    let t = findNumber(p[i],p[i+1])
-    sum+=BigInt(t)
-    i++
+  let i = 0;
+  let sum = 0n;
+  while (p[i] < 1000000) {
+    let t = findNumber(p[i], p[i + 1]);
+    sum += BigInt(t);
+    i++;
   }
-  return sum
+  return sum;
 };
 
 console.log(p134Solution());
-
