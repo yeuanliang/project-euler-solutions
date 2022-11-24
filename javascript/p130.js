0ed430228e0fcbffc05541fe95d6c15f3c236735
zizaiziran <zizaiziran@gmail.com>
2022-11-24 14:40:45 +0800

    refactor

--- javascript/p130.js
+++ javascript/p130.js
@@ -6,22 +6,22 @@ const helper = require("./helper");
 
 const repunits = new Map();
 
-const findRepunitComposite = function (n) {
-  const { bases, exponents } = helper.primeFactorization(n);
-  const len = bases.length;
-  const ks = [];
-  for (let i = 0; i < len; i++) {
-    let k = repunits.get(bases[i]);
-    ks.push(k);
-  }
-  let res = ks[0] * bases[0] ** (exponents[0] - 1);
-  for (let i = 1; i < len; i++) {
-    let t = ks[i] * bases[i] ** (exponents[i] - 1);
-    let gcd = helper.gcd(res, t);
-    res = (res * t) / gcd;
-  }
-  return res;
-};
+// const findRepunitComposite = function (n) {
+//   const { bases, exponents } = helper.primeFactorization(n);
+//   const len = bases.length;
+//   const ks = [];
+//   for (let i = 0; i < len; i++) {
+//     let k = repunits.get(bases[i]);
+//     ks.push(k);
+//   }
+//   let res = ks[0] * bases[0] ** (exponents[0] - 1);
+//   for (let i = 1; i < len; i++) {
+//     let t = ks[i] * bases[i] ** (exponents[i] - 1);
+//     let gcd = helper.gcd(res, t);
+//     res = (res * t) / gcd;
+//   }
+//   return res;
+// };
 
 const p130Solution = function () {
   const primes = helper.getPrimes(100000);
@@ -39,7 +39,7 @@ const p130Solution = function () {
         c += 2;
       }
       while (c < p[i + 1]) {
-        let ck = findRepunitComposite(c);
+        let ck = helper.findRepunit(c);
         if ((c - 1) % ck === 0) {
           solutions.push(c);
           count++;
