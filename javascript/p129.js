a84b1d2ddb09aeba40614dfc73ae0050d8283d06
zizaiziran <zizaiziran@gmail.com>
2022-11-24 14:38:13 +0800

    refactor

--- javascript/p129.js
+++ javascript/p129.js
@@ -8,24 +8,9 @@ const p129Solution = function () {
     if (start % 5 === 0) {
       start += 2;
     }
-    if (!helper.isPrime(start)) {
-      let { bases, exponents } = helper.primeFactorization(start);
-      let len = bases.length;
-      let ks = [];
-      for (let i = 0; i < len; i++) {
-        let k = helper.findRepunit(bases[i]);
-        ks.push(k);
-      }
-      let res = ks[0] * bases[0] ** (exponents[0] - 1);
-      for (let i = 1; i < len; i++) {
-        let t = ks[i] * bases[i] ** (exponents[i] - 1);
-        let gcd = helper.gcd(res, t);
-        res = (res * t) / gcd;
-      }
-
-      if (res > 1000000) {
-        return start;
-      }
+    let k = helper.findRepunit(start);
+    if (k > 1000000) {
+      return start;
     }
     start += 2;
   }
