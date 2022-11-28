412fbefec13229a801e08be38983934a8872d1ef
zizaiziran <zizaiziran@gmail.com>
2022-11-28 13:12:51 +0800

    add new method

--- javascript/p048.js
+++ javascript/p048.js
@@ -2,12 +2,35 @@
 
 const helper = require("./helper.js");
 
-const p048Solution = function (limit) {
-  let result = [];
-  for (let i = limit; i > 0; i--) {
-    result = helper.bigNumberSum(helper.customPower(i, i), result);
+// const p048Solution = function (limit) {
+//   let result = [];
+//   for (let i = limit; i > 0; i--) {
+//     result = helper.bigNumberSum(helper.customPower(i, i), result);
+//   }
+//   return result.slice(-10);
+// };
+
+// console.log(p048Solution(1000).join(''));
+
+// n^n last ten digits
+const lastTenDigits = function (n) {
+  if(n===0||n===1){
+    return n
   }
-  return result.slice(-10);
+  let product = n;
+  for (let i = 2; i <= n; i++) {
+    product = (n * product) % 10 ** 10;
+  }
+  return product;
 };
 
-console.log(p048Solution(1000));
+const p048Solution2 = function (limit) {
+  let sum = 0;
+  for (let i = 1; i <= limit; i++) {
+    sum += lastTenDigits(i);
+    sum = sum % 10 ** 10;
+  }
+  return sum;
+};
+
+console.log(p048Solution2(1000));
