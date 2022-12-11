d311ac9fb473e5d5e2ed3d4e010870571330d11e
zizaiziran <zizaiziran@gmail.com>
2022-12-11 18:41:59 +0800

    update

--- javascript/p223.js
+++ javascript/p223.js
@@ -6,25 +6,25 @@
 
 const helper = require("./helper");
 
-// time cost: 1h 30min 21s
+// time cost: 1h 37min 42s
 const p223Solution = function (limit) {
   const limitA = Math.floor(limit / (2 + Math.SQRT2));
   let count = Math.floor((limit - 1) / 2);
   for (let a = 2; a < limitA; a++) {
     let product = a * a - 1;
     let divisors = helper.genDivisors(product).sort((a, b) => a - b);
-    let len = divisors.length / 2;
-    for (let j = 0; j < len; j++) {
+    for (let j = 0; divisors[j] < a-1; j++) {
       let k = product / divisors[j];
-      if ((k - divisors[j]) % 2 === 0 && (k + divisors[j]) % 2 === 0) {
-        let b = (k - divisors[j]) / 2;
-        if (b < a) {
-          break;
-        }
-        let c = (k + divisors[j]) / 2;
-        if (b >= a && a + b + c <= limit) {
-          count++;
-        }
+      let b = (k - divisors[j]) / 2;
+      if (b < a) {
+        break;
+      }
+      if (!Number.isInteger(b)) {
+        continue;
+      }
+      let c = (k + divisors[j]) / 2;
+      if (a + b + c <= limit) {
+        count++;
       }
     }
   }
@@ -32,3 +32,9 @@ const p223Solution = function (limit) {
 };
 
 console.log(p223Solution(25000000));
+
+// from a triplet (a,b,c), the next 3 triplets
+// (2c + b - 2a, 2c + 2b - a, 3c + 2b - 2a)
+// (2c + b + 2a, 2c + 2b + a, 3c + 2b + 2a)
+// (2c - 2b + a, 2c - b + 2a, 3c - 2b + 2a)
+// Start from (1,1,1) and (1,2,2), be careful about duplicates when a=b
